705905a9370070cf4b3f2e707af6f120e2375dc1
Marcus Vinícius <marcus.varocha@gmail.com>
2022-09-06 23:52:38 +0000

    usuario

---
+++ src/views/UsuarioView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="usuario">
+    <h1 v-if="nome.length > 5">{{nome}}</h1>
+    <input type="text" v-model="nome">
+    <ul>
+        <li v-for="nota in notas" :key="nota">
+            <h2>{{nota}}</h2>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "usuarioView",
+  data() {
+    return {
+      nome: "Marcus",
+      notas: ['oi', 'tchau']
+    };
+  },
+};
+</script>
